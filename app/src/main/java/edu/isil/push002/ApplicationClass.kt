d530a4e35c600f41af98ff3c560e5f43ffed7c2b
maxmu <maxmuc@outlook.com>
2024-11-28 18:44:01 -0500

    Initial commit

---
+++ app/src/main/java/edu/isil/push002/ApplicationClass.kt
@@ -0,0 +1,30 @@
+package edu.isil.push002
+
+import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+import com.onesignal.OneSignal
+import com.onesignal.debug.LogLevel
+
+// NOTE: Replace the below with your own ONESIGNAL_APP_ID
+val ONESIGNAL_APP_ID = "b243d048-d908-468a-b09d-69007b781084"
+
+class ApplicationClass : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Verbose Logging set to help debug issues, remove before releasing your app.
+        OneSignal.Debug.logLevel = LogLevel.VERBOSE
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this, ONESIGNAL_APP_ID)
+
+        // requestPermission will show the native Android notification permission prompt.
+        // NOTE: It's recommended to use a OneSignal In-App Message to prompt instead.
+        CoroutineScope(Dispatchers.IO).launch {
+            OneSignal.Notifications.requestPermission(false)
+        }
+    }
+}
